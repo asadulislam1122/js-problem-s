47b83580d0575d480f21e7464bd839d824c0d37e
asadulislam <asadulislam0288@gmail.com>
2026-09-22 09:26:30 +0600

    added by even and largest

---
+++ even.js
@@ -0,0 +1,16 @@
+// problem 1
+let number = 7;
+
+if (number % 2 === 0) {
+  console.log("this is even number");
+} else {
+  console.log("this is odd number");
+}
+
+//  problem 2
+const isEven = (num) => num % 2 === 0;
+
+console.log(isEven(4)); // true
+console.log(isEven(7)); // false
+
+//  problem 3
---
+++ laregst.js
@@ -0,0 +1,11 @@
+let a = 24;
+let b = 12;
+let c = 36;
+
+if (a > b && a > c) {
+  console.log("a is the largest number");
+} else if (b > a && b > c) {
+  console.log("b is the largest number");
+} else {
+  console.log("c is the largest number");
+}
